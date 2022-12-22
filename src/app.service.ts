7db5c82d6a0f41aab16f5ed97a90a5d5cee3bded
Rob McKee <46908845+foestauf@users.noreply.github.com>
2022-12-21 19:08:55 -0500

    Add table delete route (#29)
    * Add table delete route
    
    * Add controller tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import seeder from '../prisma/seed';
 
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(private readonly prisma: PrismaService) {}
+  private logger = new Logger('AppService');
   getHello(): string {
     return 'Hello World!';
   }
 
   async onModuleInit() {
-    console.log('AppService initialized');
+    this.logger.log('AppService initialized');
     // Check if cards exist, if not seed the database
     const cards = await this.prisma.card.findMany();
     if (cards.length === 0) {
@@ -19,7 +20,7 @@ export class AppService implements OnModuleInit {
   }
 
   async seedDatabase() {
-    console.log('Seeding database');
+    this.logger.log('Seeding database');
     await seeder();
   }
 }
--- src/table/__tests__/table.controller.spec.ts
+++ src/table/__tests__/table.controller.spec.ts
@@ -26,7 +26,16 @@ describe('TableController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TableController],
       providers: [
-        TableService,
+        {
+          provide: TableService,
+          useValue: {
+            createTable: jest.fn(),
+            getTables: jest.fn(),
+            getTable: jest.fn(),
+            updateTable: jest.fn(),
+            deleteTable: jest.fn(),
+          },
+        },
         {
           provide: PrismaService,
           useValue: db,
@@ -47,5 +56,93 @@ describe('TableController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have service defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should have shoeService defined', () => {
+    expect(shoeService).toBeDefined();
+  });
+
+  describe('createTable', () => {
+    it('should call service.createTable', async () => {
+      const dto = {
+        name: 'Table 1',
+        description: 'Table 1',
+        config: {
+          minBet: 5,
+          maxBet: 500,
+          maxPlayers: 6,
+          seats: [
+            {
+              position: 1,
+              name: 'Seat 1',
+              description: 'Seat 1',
+            },
+            {
+              position: 2,
+              name: 'Seat 2',
+              description: 'Seat 2',
+            },
+          ],
+        },
+      };
+
+      await controller.createTable(dto);
+      expect(service.createTable).toBeCalledWith(dto);
+    });
+  });
+
+  describe('getTables', () => {
+    it('should call service.getTables', async () => {
+      await controller.getTables();
+      expect(service.getTables).toBeCalled();
+    });
+  });
+
+  describe('getTable', () => {
+    it('should call service.getTable', async () => {
+      await controller.getTable('1');
+      expect(service.getTable).toBeCalledWith('1');
+    });
+  });
+
+  describe('updateTable', () => {
+    it('should call service.updateTable', async () => {
+      const dto = {
+        name: 'Table 1',
+        description: 'Table 1',
+        config: {
+          minBet: 5,
+          maxBet: 500,
+          minPlayers: 2,
+          maxPlayers: 6,
+          seats: [
+            {
+              position: 1,
+              name: 'Seat 1',
+              description: 'Seat 1',
+            },
+            {
+              position: 2,
+              name: 'Seat 2',
+              description: 'Seat 2',
+            },
+          ],
+        },
+      };
+
+      await controller.updateTable('1', dto);
+      expect(service.updateTable).toBeCalledWith('1', dto);
+    });
+  });
+
+  describe('deleteTable', () => {
+    it('should call service.deleteTable', async () => {
+      await controller.deleteTable('1');
+      expect(service.deleteTable).toBeCalledWith('1');
+    });
   });
 });
